22f8be1401040439a6f00af5101b2551b3d9407d
Junaid Ansari <junaidansari21975@gmail.com>
2023-12-30 22:27:37 +0530

    my courses section done, minor modifications required.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -22,6 +22,7 @@ import { useDispatch, useSelector } from "react-redux";
 import Cart from "./components/core/Dashboard/Cart";
 import AddCourse from "./components/core/Dashboard/AddCourse";
 import MyCourses from "./components/core/Dashboard/MyCourses";
+import EditCourse from "./components/core/Dashboard/EditCourse/index";
 
 function App() {
 
@@ -114,6 +115,7 @@ function App() {
               <>
                 <Route path="dashboard/add-course" element={<AddCourse />} />
                 <Route path="dashboard/my-courses" element={<MyCourses />} />
+                <Route path="dashboard/edit-course/:courseId" element={<EditCourse />} />
               </>
             )
           }
